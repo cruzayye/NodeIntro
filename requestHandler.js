042a59bce7b48e010ca04c0116916f17de6ed569
cruzayye <johnnyc010392@gmail.com>
2019-01-13 21:01:52 -0800

    pathname in server js had to be lower cased

--- requestHandler.js
+++ requestHandler.js
@@ -1,10 +1,12 @@
 function start(){
   console.log('request handler start was called');
+  return 'Hello Start';
 
 }
 
 function upload(){
   console.log('request handler upload was called');
+  return 'Hello Upload';
 }
 
 
--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var pathName = url.parse(request.url).pathName;
+    var pathName = url.parse(request.url).pathname;
     console.log(`Request for ${pathName} received.`);
-    route(handle, pathName);
 
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello World');
+    var content = route(handle, pathName);
+
+    response.write(content);
     response.end();
   }
   
